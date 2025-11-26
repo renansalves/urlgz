68d169a8a2448f35ccd9d71d97d284251131f83a
renansalves <renan.alves@edu.pucrs.br>
2025-11-26 17:47:48 -0300

    feat
    Cria a entidade e seus atributos, utilizando as anotações do Jakarta
    para melhor controle para criação dos objetos no banco de dados.

--- src/main/java/br/urlgz/app/model/UrlEntity.java
+++ src/main/java/br/urlgz/app/model/UrlEntity.java
@@ -1,6 +1,34 @@
 package br.urlgz.app.model;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class UrlEntity {
 
-  
+ @Id
+ @GeneratedValue(strategy = GenerationType.IDENTITY)
+ private Long id;
+
+ @Column(name="created_at", nullable = false)
+ private LocalDateTime createdAt;
+
+ @Column(name="expires_at", nullable = false)
+ private LocalDateTime expiresAt;
+
+ @Column(name = "short_code",unique = true, nullable = false)
+ private String shortCode;
+
+ @Column(name="original_url", nullable = false)
+ private String originalUrl;
+ @Column(name="click_count", columnDefinition="Integer default '0'")
+ private int clickCount;
+ @Column(name="is_active", columnDefinition="Boolean default 'true'")
+ private Boolean isActive;
+
 }
