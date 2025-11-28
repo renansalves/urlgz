c75cad19caee3d661dee867ac250db96a8fc1858
renansalves <renan.alves@edu.pucrs.br>
2025-11-28 10:44:50 -0300

    Feat: Classe de serviço, estrutura inicial. Criação da classe de testes para acompanhar o desenvolvimento (TDD).

--- src/main/java/br/urlgz/app/service/UrlService.java
+++ src/main/java/br/urlgz/app/service/UrlService.java
@@ -1,13 +1,96 @@
 package br.urlgz.app.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.urlgz.app.dto.UrlResponse;
+import br.urlgz.app.utils.Base62;
+
+/**
+ * Classe UrlService, responsavel em proverer as principais funcionalidades para
+ * encurtar urls.
+ * 
+ * @author Renan Alves
+ * @version 1.0
+ */
 @Service
 public class UrlService {
 
-    public static void urlEncode(String url) {
-      // codifica url para short url
-    }
+  @Autowired
+  private UrlResponse urlResponse;
+  @Autowired
+  private Base62 base62;
 
-  
+  /**
+   * Metodo responsavel pela codificação da URL.
+   * Realiza a codificação da url utilizando a classe base62
+   * @see {@link br.urlgz.app.utils.Base62}
+   * 
+   * @author Renan Alves
+   *
+   * @param url String - String contendo a url a ser encurtada.
+   * @param expiresIn LocalDateTime- Data de expiração do link encurtado.
+   *
+   * <p>
+   * Valor de retorno da chamada POST /api/v1/shorten.
+   * <pre>{@code
+   *  {
+   *    "url": "https://exemplo.com/pagina-muito-longa",
+   *    "expiresIn": "30d" // opcional
+   *  }
+   * }</pre>
+   *
+   */
+  public UrlResponse urlShortEncode(String url) {
+
+    // Deve encurtar a url e salvar os dados no banco.
+    long id = 0;
+    String urlResponse = base62.encode(id);
+
+    return null;
+
+  }
+
+  /**
+   * Metodo responsavel pela consulta da URL curta, e redirecionamento da url original.
+   * 
+   * @author Renan Alves
+   * @param shortCode String - String contendo a url a ser encurtada.
+   * @param originalUrl String - Data de expiração do link encurtado.
+   * @param createdAt LocalDateTime - Data de expiração do link encurtado.
+   * @param totalClicks longlong - Data de expiração do link encurtado.
+   * @param clicksLast30Days Long - Data de expiração do link encurtado.
+   * ### **GET /api/urls/{shortCode}**
+   * @return
+   * ```json
+   *  {
+   *    "shortCode": "abc123",
+   *    "originalUrl": "https://...",
+   *    "createdAt": "2024-01-01T00:00:00Z",
+   *    "totalClicks": 150,
+   *    "clicksLast30Days": 45
+   *  }
+   *  ```
+   */
+
+  public UrlResponse searchOriginalUrl(String shortUrl) {
+
+    return null;
+  }
+
+
+  /**
+   * Metodo responsavel pela remoção dos registros no banco.
+   * 
+   * @author Renan Alves
+   * @param shortCode String - String contendo a url a ser encurtada.
+   * ```json
+   *  {
+   *    "shortCode": "abc123",
+   *  }
+   *  ```
+   */
+  public void deleteShortlUrlData(Long id){
+
+  }
 }
--- src/test/java/br/urlgz/app/service/UrlServiceTest.java
+++ src/test/java/br/urlgz/app/service/UrlServiceTest.java
@@ -1,9 +1,16 @@
 package br.urlgz.app.service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import br.urlgz.app.dto.*;
 
 public class UrlServiceTest {
 
+  @Autowired
+  private UrlResponse urlResponse;
+  @Autowired
+  private UrlService urlService;
 // Testes da service, função salvar, consultar, deletar. Função hash para criação da url encurtada e unica.
 // 1 - Teste função Hash
 // 2 - Teste função Salvar
@@ -13,10 +20,13 @@ public class UrlServiceTest {
 
 // shortcode -> 5 caracteres 62^5 -> 916.132.832 URLS distintas
 // Função hash, hasids
+  
   @Test
-  DeveTransformarUrlLongaEmCurta(){
+  void DeveTransformarUrlLongaEmCurta(){
     final String url = "www.google.com";
-    UrlService.urlEncode(url);
-
+    
+    // deve retornar o objeto 
+    urlResponse = urlService.urlShortEncode(url);
+    
   }
 }
