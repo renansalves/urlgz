97f49ae7577f6ba288128c824ee706548193a0ca
renansalves <renan.alves@edu.pucrs.br>
2025-11-28 12:10:01 -0300

    Feat
    interface repository extendendo a interface JpaRepository.

---
+++ src/main/java/br/urlgz/app/repository/UrlRepository.java
@@ -0,0 +1,9 @@
+package br.urlgz.app.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import br.urlgz.app.model.UrlEntity;
+
+public interface UrlRepository extends JpaRepository<UrlEntity, Long> {
+
+  
+}
